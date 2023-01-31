3ae45b6dea70793ce1d8477607f4f6437b90cecb
cuber-zjd <1046539941@qq.com>
2023-01-31 23:58:47 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/test/java/com/cuber/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuber.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
